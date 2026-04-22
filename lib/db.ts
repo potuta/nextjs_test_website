37a5e14a1f2b3fceda56da56a1095bd8caaf81f3
potuta <cyrilbautista2@gmail.com>
2026-04-22 15:16:32 +0800

    configured prisma for deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,17 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
 
-export { prisma as db };
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter, 
+  }); 
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export { prisma as db }; 
